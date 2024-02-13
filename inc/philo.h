c47b71e888b57a5755f5c5c2102e98d6b3d7d2e7
Kathryn Sansom <ksansom@c4c5c4.42berlin.de>
2024-02-13 11:17:39 +0100

    first structure

---
+++ inc/philo.h
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ksansom <ksansom@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/13 11:03:10 by ksansom           #+#    #+#             */
+/*   Updated: 2024/02/13 11:15:47 by ksansom          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+
+# include <string.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <sys/types.h>
+# include <signal.h>
+# include <pthread.h> 
+# include <sys/time.h>
+# include <sys/wait.h>
+# include <fcntl.h>
+# include <sys/stat.h>
+# include <semaphore.h>
+
+
+
+
+
+#endif
