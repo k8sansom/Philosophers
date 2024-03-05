e23904ca13b83f3a1e61785f78dbea2631b5b755
Kate <100698139+k8sansom@users.noreply.github.com>
2024-03-05 10:45:43 +0100

    Update routines.c

--- philo/src/routines.c
+++ philo/src/routines.c
@@ -38,7 +38,7 @@ void	*routine(void *philo_ptr)
 	philo = (t_philosopher *)philo_ptr;
 	ft_update_meal_time(philo);
 	if (philo->id % 2 == 0)
-		ft_usleep(philo->data->time_eat - 2);
+		ft_usleep(philo->data->time_eat - 10);
 	while (get_philo_state(philo) != DEAD)
 	{
 		if (ft_eat(philo) != 0)
